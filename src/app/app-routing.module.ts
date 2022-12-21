820152c9a4b8737700bb1c4c6f3589c4bbbde233
Eduardo P <edtvports@gmail.com>
2022-12-21 18:35:13 -0300

    Navbar, routes and structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { YouComponent } from './pages/you/you.component';
+import { SettingsComponent } from "./pages/settings/settings.component";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'you', component: YouComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { YouComponent } from './pages/you/you.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ExerciseCardComponent } from './components/exercise-card/exercise-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    YouComponent,
+    SettingsComponent,
+    ExerciseCardComponent
   ],
   imports: [
     BrowserModule,
