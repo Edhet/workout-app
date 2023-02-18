888199afdb26e4c1056d336a4f96b5a9e7e36a62
Eduardo P <edtvports@gmail.com>
2023-02-18 15:42:35 -0300

    doesn't send exercise info to firebase anymore

--- src/app/pages/edit-schedule/edit-schedule.component.ts
+++ src/app/pages/edit-schedule/edit-schedule.component.ts
@@ -85,6 +85,7 @@ export class EditScheduleComponent implements OnInit {
     if (!answer)
       this.confirmationPromptOpen = false;
     this.confirmationPromptOpen = false;
+    this.scheduleToEdit?.exercises.forEach(exercises => exercises.exercisesInfo = undefined);
     await this.router.navigate(["you"]);
     await this.schedulesService.updateSchedule(this.scheduleToEdit!, this.scheduleID!);
   }
